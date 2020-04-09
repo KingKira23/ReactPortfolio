80c53b196c2708c1e755e83222aec4432a6023f2
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-04-08 23:09:01 -0500

    Updates

---
+++ precache-manifest.caac4d1bbd244d0cc5cb448af3240cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8363b7c379b9403dd3fec25dc4d5b04",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "ba02cf07e3821d6449c8",
+    "url": "/ReactPortfolio/static/css/main.f67de4ce.chunk.css"
+  },
+  {
+    "revision": "6105c75ad9ba42abcf40",
+    "url": "/ReactPortfolio/static/js/2.a1505fe4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.a1505fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba02cf07e3821d6449c8",
+    "url": "/ReactPortfolio/static/js/main.0a07fd1f.chunk.js"
+  },
+  {
+    "revision": "2a5f3aa8c9b99516ef11",
+    "url": "/ReactPortfolio/static/js/runtime-main.e5dedc1b.js"
+  }
+]);
